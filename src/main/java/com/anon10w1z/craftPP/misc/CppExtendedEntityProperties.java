9b84f424f5174300d17462cda29a2bc40ef12b94
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-03-24 19:07:45 -0800

    Revert "Improved extended properties/build.gradle"
    This reverts commit 6e110c44abbf825ff4b3b1ab73646eefdf0ae5f9.

--- src/main/java/com/anon10w1z/craftPP/misc/CppExtendedEntityProperties.java
+++ src/main/java/com/anon10w1z/craftPP/misc/CppExtendedEntityProperties.java
@@ -23,13 +23,16 @@ public class CppExtendedEntityProperties implements IExtendedEntityProperties {
 	public CppExtendedEntityProperties(EntityItem entityItem) {
 		this.entityItem = entityItem;
 		this.world = entityItem.worldObj;
-		this.minSteadyTicks = this.world.rand.nextInt(51) + 50; //2.5 to 5 seconds
 	}
 
-	/**
-	 * Saves the extended properties to NBT
-	 * @param compound The NBT tag compound to write the properties to
-	 */
+	public static void registerExtendedProperties(EntityItem entityItem) {
+		entityItem.registerExtendedProperties(EXTENDED_PROPERTIES_NAME, new CppExtendedEntityProperties(entityItem));
+	}
+
+	public static CppExtendedEntityProperties getExtendedProperties(EntityItem entityItem) {
+		return (CppExtendedEntityProperties) entityItem.getExtendedProperties(EXTENDED_PROPERTIES_NAME);
+	}
+
 	@Override
 	public void saveNBTData(NBTTagCompound compound) {
 		NBTTagCompound properties = new NBTTagCompound();
@@ -38,10 +41,6 @@ public class CppExtendedEntityProperties implements IExtendedEntityProperties {
 		compound.setTag(EXTENDED_PROPERTIES_NAME, properties);
 	}
 
-	/**
-	 * Loads the extended properties from NBT
-	 * @param compound The NBT tag compound to read the properties from
-	 */
 	@Override
 	public void loadNBTData(NBTTagCompound compound) {
 		NBTTagCompound properties = (NBTTagCompound) compound.getTag(EXTENDED_PROPERTIES_NAME);
@@ -54,28 +53,11 @@ public class CppExtendedEntityProperties implements IExtendedEntityProperties {
 		//do nothing
 	}
 
-	/**
-	 * Registers the extended properties for the given item entity
-	 * @param entityItem The item entity to register the properties for
-	 */
-	public static void registerExtendedProperties(EntityItem entityItem) {
-		entityItem.registerExtendedProperties(EXTENDED_PROPERTIES_NAME, new CppExtendedEntityProperties(entityItem));
-	}
-
-	/**
-	 * Returns the extended properties for the given item entity
-	 * @param entityItem The item entity to obtain the properties from
-	 * @return The CppExtendedProperties of the given item entity
-	 */
-	public static CppExtendedEntityProperties getExtendedProperties(EntityItem entityItem) {
-		return (CppExtendedEntityProperties) entityItem.getExtendedProperties(EXTENDED_PROPERTIES_NAME);
-	}
-
-	/**
-	 * Handles all automatic seed planting logic
-	 */
 	public void handlePlantingLogic() {
+		if (this.minSteadyTicks < 50)
+			this.minSteadyTicks = this.entityItem.worldObj.rand.nextInt(51) + 50;
 		++this.steadyTicks;
+
 		BlockPos entityPosDown = new BlockPos(this.entityItem).down();
 		BlockPos lastTickEntityPosDown = new BlockPos(this.entityItem.lastTickPosX, this.entityItem.lastTickPosY, this.entityItem.lastTickPosZ).down();
 		if (entityPosDown.compareTo(lastTickEntityPosDown) != 0)
