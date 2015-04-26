ea8856f195798206ea5d6719625e81405607f49b
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-04-25 16:10:38 -0800

    Fixed some problems with build files

--- src/main/java/io/github/anon10w1z/craftPP/main/CppModInfo.java
+++ src/main/java/io/github/anon10w1z/craftPP/main/CppModInfo.java
@@ -17,7 +17,7 @@ public final class CppModInfo {
 	/**
 	 * Craft++'s version
 	 */
-	public static final String VERSION = "@VERSION@";
+	public static final String VERSION = "2.3.2";
 
 	/**
 	 * Craft++'s package location
