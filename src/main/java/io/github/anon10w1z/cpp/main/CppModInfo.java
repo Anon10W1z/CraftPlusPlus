c266aff51c68b5ca0d67e00a1a40156e48bd60fb
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-08-05 12:50:20 -0700

    Version 3.6.8 Fixes only the Craft++ coremod loading and not the actual mod

--- src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
+++ src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
@@ -17,7 +17,7 @@ public final class CppModInfo {
 	/**
 	 * Craft++'s version
 	 */
-	public static final String VERSION = "3.6.7";
+	public static final String VERSION = "3.6.8";
 
 	/**
 	 * Craft++'s package location
