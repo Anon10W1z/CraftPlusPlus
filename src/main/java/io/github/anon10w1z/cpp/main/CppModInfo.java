1085020ae60eea0ab294692f7ec14ac0dde6a02c
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-08-05 10:27:46 -0700

    Version 3.6.7 Removes the self-installer.\nI will replace it with a standalone installer.

--- src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
+++ src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
@@ -17,7 +17,7 @@ public final class CppModInfo {
 	/**
 	 * Craft++'s version
 	 */
-	public static final String VERSION = "3.6.6";
+	public static final String VERSION = "3.6.7";
 
 	/**
 	 * Craft++'s package location
