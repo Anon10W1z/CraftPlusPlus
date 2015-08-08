22cf2683314f3e5ba032f13f3b0884bbfd0cee47
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-08-08 12:18:24 -0700

    Version 3.6.9 Nimble and Hops are now identical to their potion counterparts

--- src/main/java/io/github/anon10w1z/cpp/enchantments/EnchantmentNimble.java
+++ src/main/java/io/github/anon10w1z/cpp/enchantments/EnchantmentNimble.java
@@ -58,7 +58,7 @@ public class EnchantmentNimble extends CppEnchantmentBase {
 	private void addSpeedBuff(EntityLivingBase livingEntity, int enchantmentLevel) {
 		IAttributeInstance speedAttribute = livingEntity.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.movementSpeed);
 		if (speedAttribute.getModifier(nimbleUUID) == null) {
-			AttributeModifier speedModifier = new AttributeModifier(nimbleUUID, "Nimble", (float) enchantmentLevel * 0.20000000298023224, 1);
+			AttributeModifier speedModifier = new AttributeModifier(nimbleUUID, "Nimble", (float) enchantmentLevel * 0.20000000298023224, 2);
 			speedAttribute.applyModifier(speedModifier);
 		}
 	}
@@ -72,7 +72,7 @@ public class EnchantmentNimble extends CppEnchantmentBase {
 	private void removeSpeedBuff(EntityLivingBase livingEntity, int enchantmentLevel) {
 		IAttributeInstance speedAttribute = livingEntity.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.movementSpeed);
 		if (speedAttribute.getModifier(nimbleUUID) != null) {
-			AttributeModifier speedModifier = new AttributeModifier(nimbleUUID, "Nimble", (float) enchantmentLevel * 0.20000000298023224, 1);
+			AttributeModifier speedModifier = new AttributeModifier(nimbleUUID, "Nimble", (float) enchantmentLevel * 0.20000000298023224, 2);
 			speedAttribute.removeModifier(speedModifier);
 		}
 	}
--- src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
+++ src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
@@ -17,7 +17,7 @@ public final class CppModInfo {
 	/**
 	 * Craft++'s version
 	 */
-	public static final String VERSION = "3.6.8";
+	public static final String VERSION = "3.6.9";
 
 	/**
 	 * Craft++'s package location
