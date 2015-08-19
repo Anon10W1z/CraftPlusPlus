c6d7faf0d94b0ae8bb8e60b708d266cddf24e76b
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-08-19 16:48:21 -0700

    Fix version number

--- src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
+++ src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
@@ -17,7 +17,7 @@ public final class CppModInfo {
 	/**
 	 * Craft++'s version
 	 */
-	public static final String VERSION = "3.6.14";
+	public static final String VERSION = "3.7";
 
 	/**
 	 * Craft++'s package location
