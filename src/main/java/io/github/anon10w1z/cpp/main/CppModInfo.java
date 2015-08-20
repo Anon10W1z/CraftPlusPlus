2ed0acc9cff0e098ba8f4ce6f40bddab40a7271e
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-08-20 12:30:20 -0700

    Version 3.7.2 Extends potion and enchantment localization to 255, and enhances potion display

--- src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
+++ src/main/java/io/github/anon10w1z/cpp/main/CppModInfo.java
@@ -17,7 +17,7 @@ public final class CppModInfo {
 	/**
 	 * Craft++'s version
 	 */
-	public static final String VERSION = "3.7.1";
+	public static final String VERSION = "3.7.2";
 
 	/**
 	 * Craft++'s package location
--- src/main/java/io/github/anon10w1z/cpp/proxies/CppClientProxy.java
+++ src/main/java/io/github/anon10w1z/cpp/proxies/CppClientProxy.java
@@ -6,7 +6,9 @@ import io.github.anon10w1z.cpp.items.CppItems;
 import io.github.anon10w1z.cpp.main.CppModInfo;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.renderer.entity.RenderSnowball;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.client.settings.KeyBinding;
@@ -19,7 +21,9 @@ import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * The client-side proxy for Craft++
@@ -54,23 +58,35 @@ public class CppClientProxy extends CppCommonProxy {
 	public void displayPotionEffects() {
 		int xPos = 2;
 		int yPos = 2;
-		Collection<PotionEffect> potionEffects = minecraft.thePlayer.getActivePotionEffects();
+		List<PotionEffect> potionEffects = new ArrayList<>(minecraft.thePlayer.getActivePotionEffects());
+		Collections.sort(potionEffects, (potionEffect1, potionEffect2) -> potionEffect1.getDuration() - potionEffect2.getDuration());
+		Gui gui = new Gui();
 		for (PotionEffect potionEffect : potionEffects) {
 			Potion potion = Potion.potionTypes[potionEffect.getPotionID()];
 			int potionDuration = potionEffect.getDuration();
-			if (!potion.hasStatusIcon() || (potionDuration <= 200 && potionDuration >= 60 && potionDuration % 20 >= 10) || (potionDuration <= 60 && potionDuration % 10 >= 5))
+			if (!potion.hasStatusIcon())
 				continue;
 			GL11.glColor4f(1, 1, 1, 1);
 			GL11.glDisable(GL11.GL_LIGHTING);
 			minecraft.renderEngine.bindTexture(new ResourceLocation("textures/gui/container/inventory.png")); //draw from inventory.png
 			int iconIndex = potion.getStatusIconIndex();
-			//constants for drawing
+			//constants for drawing/loading potion icons from inventory.png
 			final int POTION_ICON_SIZE = 18;
 			final int POTION_ICON_BASE_X_OFFSET = 0;
 			final int POTION_ICON_BASE_Y_OFFSET = 198;
 			final int POTION_ICONS_PER_ROW = 8;
 			final int POTION_ICON_SPACING = POTION_ICON_SIZE + 2;
-			new Gui().drawTexturedModalRect(xPos, yPos, POTION_ICON_BASE_X_OFFSET + iconIndex % POTION_ICONS_PER_ROW * POTION_ICON_SIZE, POTION_ICON_BASE_Y_OFFSET + iconIndex / POTION_ICONS_PER_ROW * POTION_ICON_SIZE, POTION_ICON_SIZE, POTION_ICON_SIZE);
+			if ((potionDuration > 200 || potionDuration <= 60 || potionDuration % 20 < 10) && (potionDuration > 60 || potionDuration % 10 < 5)) {
+				GL11.glPushMatrix();
+				GL11.glScalef(8 / 9F, 8 / 9F, 1);
+				gui.drawTexturedModalRect(xPos, yPos, POTION_ICON_BASE_X_OFFSET + iconIndex % POTION_ICONS_PER_ROW * POTION_ICON_SIZE, POTION_ICON_BASE_Y_OFFSET + iconIndex / POTION_ICONS_PER_ROW * POTION_ICON_SIZE, POTION_ICON_SIZE, POTION_ICON_SIZE);
+				GL11.glPopMatrix();
+				if (potionEffect.getAmplifier() > 0) {
+					FontRenderer fontRenderer = minecraft.fontRendererObj;
+					String amplifierString = Integer.toString(potionEffect.getAmplifier() + 1);
+					gui.drawString(fontRenderer, amplifierString, xPos + 17 - fontRenderer.getStringWidth(amplifierString), yPos + 9, 0xFFFFFF);
+				}
+			}
 			xPos += POTION_ICON_SPACING;
 		}
 	}
@@ -88,7 +104,7 @@ public class CppClientProxy extends CppCommonProxy {
 
 	@Override
 	public boolean isGuiOpen() {
-		return minecraft.currentScreen != null;
+		return minecraft.currentScreen != null && !(minecraft.currentScreen instanceof GuiChat);
 	}
 
 	private void registerItemInventoryRenderer(Item item, String name) {
